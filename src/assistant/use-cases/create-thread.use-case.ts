a839908f3c56fdc677e6f0d3270ab642577a8a0d
jlazaro <jlazaro135@gmail.com>
2024-01-31 17:41:11 +0100

    se finaliza nest

--- src/assistant/use-cases/create-thread.use-case.ts
+++ src/assistant/use-cases/create-thread.use-case.ts
@@ -1,8 +1,15 @@
 import OpenAI from "openai";
+import * as fs from 'fs'
 
 
 export const createThreadUseCase = async(openai: OpenAI) => {
     const {id} = await openai.beta.threads.create();
+
+    const file = await openai.files.create({
+        file: fs.createReadStream("mydata.csv"),
+        purpose: "assistants",
+      });
+    
 
     return {id};
 }
